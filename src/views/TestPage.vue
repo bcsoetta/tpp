8e438c4c792d662feeabf4128c83fc0327310af7
Bow Vernon <bowvernon110391@gmail.com>
2020-07-03 07:42:21 +0700

    Setup TestPage for upload data awal

--- src/views/TestPage.vue
+++ src/views/TestPage.vue
@@ -1,21 +1,34 @@
 <template>
   <div>
-    <b-form-group
-    description="Ukuran file max 16 Mb"
-    >
+    <b-form-group description="Ukuran file max 16 Mb">
       <template #label>
-        Browse File Excel (<a href="/static/assets/contoh-data-awal-gudang.xlsx">Contoh File Excel</a>)
+        Browse File Excel (
+        <a href="/static/assets/contoh-data-awal-gudang.xlsx">Contoh File Excel</a>)
       </template>
       <!-- body -->
-      <b-form-file 
-        v-model="excelFile" size="sm"
+      <b-form-file
+        v-model="excelFile"
+        size="sm"
         :state="Boolean(excelFile)"
         placeholder="Pilih/Drop file excel di sini..."
         drop-placeholder="Drop file excel di sini..."
         accept=".xls, .xlsx"
         :disabled="busy"
-        />
+      />
     </b-form-group>
+    <!-- progress bar -->
+    <b-progress
+      v-if="busy"
+      :max="100"
+      show-progress
+      variant="danger"
+      animated
+      striped
+      class="shadow border border-dark"
+      height="1.5rem"
+    >
+      <b-progress-bar :value="uploading" :label="`Uploading: ${uploading.toFixed(2)} %`" />
+    </b-progress>
   </div>
 </template>
 
@@ -25,13 +38,26 @@ export default {
     return {
       excelFile: null,
       uploading: 0
-    }
+    };
   },
 
   computed: {
     busy() {
-      return (this.uploading > 0 && this.uploading < 100) || this.excelFile
+      return this.uploading > 0 && this.uploading < 100;
+    }
+  },
+
+  watch: {
+    excelFile: {
+      handler(nv) {
+        if (nv) {
+          // this.uploading = 20;
+          setInterval(() => {
+            this.uploading = 1 + Math.random() * 99.0
+          }, 1000)
+        }
+      }
     }
   }
-}
+};
 </script>
