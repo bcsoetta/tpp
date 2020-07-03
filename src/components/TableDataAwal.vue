3969ae0b9d565c9115dbee218c69bb1290ceb81e
Bow Vernon <bowvernon110391@gmail.com>
2020-07-04 00:08:22 +0700

    Add TableDataAwal to render imported excel file before being uploaded

---
+++ src/components/TableDataAwal.vue
@@ -0,0 +1,196 @@
+<template>
+  <!-- table here -->
+  <b-table
+    id="table-data-awal"
+    v-b-visible.once="e => { tableReady = e}"
+    small
+    striped
+    hover
+    bordered
+    head-variant="dark"
+    class="shadow"
+    :fields="fields"
+    responsive
+    no-border-collapse
+    v-bind="$attrs"
+    v-on="$listeners"
+  >
+    <!-- custom cell rendering -->
+    <template #cell(index)="row">{{ row.index+1 }}</template>
+
+    <!-- Nama + Alamat Importir -->
+    <template #cell(nama_importir)="row">
+      <b-form-input
+        :debounce="debounce"
+        size="sm"
+        v-model="row.item.nama_importir"
+        style="background-color: rgba(0,120,255,0.1)"
+      />
+      <b-form-textarea
+        :debounce="debounce"
+        size="sm"
+        v-model="row.item.alamat_importir"
+        style="background-color: rgba(255,50,0,0.1)"
+      />
+    </template>
+
+    <template #head(nama_importir)>
+      Nama Importir
+      <hr style="border-color: white" class="my-0" />Alamat Importir
+    </template>
+
+    <!-- Keterangan (full) -->
+    <template #cell(keterangan_rows)="row">
+      <div v-for="(k, id) in row.item.keterangan.data" :key="row.index+'_ket_'+id">
+        <b-form-textarea :debounce="debounce" size="sm" v-model="k.keterangan" />
+      </div>
+    </template>
+
+    <!-- No BC11 -->
+    <template #cell(no_bc11)="row">
+      <b-form-input :debounce="debounce" size="sm" v-model="row.item.no_bc11" style="width: 60px" />
+      <!-- Tgl BC11 -->
+      <datepicker v-model="row.item.tgl_bc11" size="sm" style="width: 130px" />
+      <b-row no-gutters style="width: 150px">
+        <b-col md="4">
+          <b-form-input :debounce="debounce" size="sm" v-model="row.item.pos" />
+        </b-col>
+        <b-col md="4">
+          <b-form-input :debounce="debounce" size="sm" v-model="row.item.subpos" />
+        </b-col>
+        <b-col md="4">
+          <b-form-input :debounce="debounce" size="sm" v-model="row.item.subsubpos" />
+        </b-col>
+      </b-row>
+    </template>
+
+    <template #head(no_bc11)>Pos BC11</template>
+
+    <!-- Kode Flight -->
+    <template #cell(kd_flight)="row">
+      <div class="text-center">
+        <b-form-input
+          :debounce="debounce"
+          size="sm"
+          v-model="row.item.kd_flight"
+          class="d-inline-block"
+          style="width: 90px"
+        />
+      </div>
+    </template>
+
+    <!-- Koli -->
+    <template #cell(koli)="row">
+      <div class="text-center">
+        <b-form-input
+          :debounce="debounce"
+          size="sm"
+          v-model="row.item.koli"
+          style="width: 50px"
+          class="d-inline-block"
+        />
+      </div>
+    </template>
+
+    <!-- Bruto -->
+    <template #cell(brutto)="row">
+      <div class="text-center">
+        <b-form-input
+          :debounce="debounce"
+          size="sm"
+          v-model="row.item.brutto"
+          class="d-inline-block"
+          style="width: 80px"
+        />
+      </div>
+    </template>
+
+    <template #head(brutto)>
+      Berat
+      <br />(Kg)
+    </template>
+
+    <!-- MAWB -->
+    <template #cell(mawb)="row">
+      <b-form-input :debounce="debounce" size="sm" v-model="row.item.mawb" />
+      <b-form-input :debounce="debounce" size="sm" v-model="row.item.hawb" />
+    </template>
+
+    <template #head(mawb)>
+      MAWB
+      <hr style="border-color: white" class="my-0" />HAWB
+    </template>
+
+    <!-- BARANG -->
+    <template #cell(barang)="row">
+      <div class="text-center" v-for="(barang, id) in row.item.barang.data" :key="row.index+'_brg_'+id">
+        <hr v-if="id > 0"/>
+        <b-row no-gutters style="max-width: 200px" class="mx-auto">
+          <!-- jumlah jenis di row pertama? -->
+          <b-col md="4">
+            <b-form-input :debounce="debounce" size="sm" v-model="barang.jumlah"/>
+          </b-col>
+          <b-col md="4">
+            <b-form-input :debounce="debounce" size="sm" v-model="barang.jenis"/>
+          </b-col>
+
+          <!-- Uraian di bawah -->
+          <b-col md="12">
+            <b-form-textarea :debounce="debounce" rows="2" size="sm" v-model="barang.uraian" />
+          </b-col>
+        </b-row>
+      </div>
+    </template>
+
+    <template #head(barang)>
+        Jumlah Jenis Barang
+    </template>
+
+  </b-table>
+</template>
+
+<script>
+import Datepicker from "@/components/Datepicker";
+
+export default {
+  inheritAttrs: false,
+
+  components: {
+    Datepicker
+  },
+
+  data() {
+    return {
+      debounce: 500
+    };
+  },
+
+  computed: {
+    fields() {
+      return [
+        {
+          label: "No",
+          key: "index"
+        },
+        "no_bc11",
+        {
+          label: "Kode Flight",
+          key: "kd_flight"
+        },
+        "koli",
+        "brutto",
+        "mawb",
+        "nama_importir",
+        {
+          label: "Barang",
+          key: "barang"
+        },
+        {
+          label: "Keterangan",
+          key: "keterangan_rows"
+        }
+      ];
+    }
+  }
+};
+</script>
--- src/views/TestPage.vue
+++ src/views/TestPage.vue
@@ -15,6 +15,7 @@
         accept=".xls, .xlsx"
         :disabled="busy"
       />
+      <!-- {{ tableReady }} -->
     </b-form-group>
     <!-- progress bar -->
     <b-progress
@@ -29,19 +30,107 @@
     >
       <b-progress-bar :value="uploading" :label="`Uploading: ${uploading.toFixed(2)} %`" />
     </b-progress>
+
+    <b-button
+      size="sm"
+      class="shadow"
+      v-if="result"
+      variant="primary"
+      v-b-modal.modal-data-awal
+    >Show Data</b-button>
+
+    <b-button
+      size="sm"
+      class="shadow"
+      v-if="result"
+      variant="danger"
+      @click="result = null"
+    >Clear Data</b-button>
+
+    <!-- Table processing indicator -->
+    <div v-if="!busy && excelFile">
+      <hr />
+      <!-- show spinner while table is being prepared? -->
+      <b-alert variant="warning" :show="!tableReady" class="text-center">
+        <b-spinner small />Preparing table
+        <b-spinner small />
+      </b-alert>
+    </div>
+
+    <!-- Wrap it in a modal? -->
+    <b-modal
+      id="modal-data-awal"
+      header-bg-variant="light"
+      footer-bg-variant="light"
+      v-model="showModal"
+      size="xl"
+      dialog-class="fullscreen"
+      scrollable
+    >
+      <!-- MODAL HEADER -->
+      <template #modal-header>
+        <h5>
+          <font-awesome-icon icon="file-excel" />
+          Import Data Awal (Excel) | Rows: {{ result.length }}
+        </h5>
+
+        
+        <b-form-group label="TPS Asal" label-cols-md="4" style="min-width: 240px">
+          <b-form-select >
+            <option value="2">Some shit</option>
+            <option value="3">Some Other shit</option>
+          </b-form-select>
+        </b-form-group>
+      </template>
+
+      <!-- MODAL FOOTER -->
+      <template #modal-footer="{ cancel }">
+        <b-button variant="danger" size="sm" @click="cancel">
+          <font-awesome-icon icon="times"/>
+          Cancel
+        </b-button>
+        <b-button variant="primary" size="sm" @click="cancel">
+          <font-awesome-icon icon="save"/>
+          Upload
+        </b-button>
+      </template>
+
+      <div v-if="result">
+        <table-data-awal :items="result" />
+
+        <!-- <pre>{{ JSON.stringify(result, null, 2) }}</pre> -->
+      </div>
+    </b-modal>
   </div>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
+import axiosErrorHandler from "../mixins/axiosErrorHandler";
+
+import TableDataAwal from "@/components/TableDataAwal";
+
 export default {
+  mixins: [axiosErrorHandler],
+
+  components: {
+    TableDataAwal
+  },
+
   data() {
     return {
       excelFile: null,
-      uploading: 0
+      uploading: 0,
+      result: null,
+      tableReady: false,
+      showAlert: false,
+      showModal: false
     };
   },
 
   computed: {
+    ...mapGetters(["api"]),
+
     busy() {
       return this.uploading > 0 && this.uploading < 100;
     }
@@ -51,13 +140,40 @@ export default {
     excelFile: {
       handler(nv) {
         if (nv) {
-          // this.uploading = 20;
-          setInterval(() => {
-            this.uploading = 1 + Math.random() * 99.0
-          }, 1000)
+          this.uploading = 1;
+          this.tableReady = false;
+          this.result = null;
+          this.showAlert = false;
+          // call the api?
+          this.api
+            .attachRawFileToUri("/excel/dataawal", this.excelFile, e => {
+              this.uploading = Math.round((e.loaded * 100) / e.total);
+            })
+            .then(e => {
+              this.showModal = true;
+              this.showAlert = true;
+              this.result = e.data.data;
+              console.log(e.data);
+
+              // remove file and set to 0
+              this.excelFile = null;
+              this.uploading = 0;
+            })
+            .catch(e => {
+              this.handleError(e);
+              // remove file and set to 0
+              this.excelFile = null;
+              this.uploading = 0;
+            });
         }
       }
     }
   }
 };
 </script>
+
+<style>
+.fullscreen.modal-dialog {
+  max-width: 100% !important;
+}
+</style>
