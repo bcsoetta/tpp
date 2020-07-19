ce6345a05ec08506103743fe9263bf34c6b2ce41
Bow Vernon <bowvernon110391@gmail.com>
2020-07-19 20:41:32 +0700

    Change badgeVariant function so it's distributed evenly

--- src/mixins/niceties.js
+++ src/mixins/niceties.js
@@ -3,14 +3,21 @@ export default {
         badgeVariant(text) {
             // make it a number?
             const variants = [
-                'primary', 'secondary', 'success', 'danger', 'warning', 'info', 'light', 'dark'
+                'primary', 
+                'secondary', 
+                'success', 
+                'danger', 
+                'warning', 
+                'info', 
+                'light', 
+                'dark'
             ];
 
             var n = 0;
             for (var i = 0; i < text.length; i++) {
-                n += text.charCodeAt(i) * Math.pow(2, i)
+                n += text.charCodeAt(i) * ((i*2)+1)
             }
-            n = n % variants.length
+            n = (n+3) % variants.length
             return variants[n]
         }
     }
