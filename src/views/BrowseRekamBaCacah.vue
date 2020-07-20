8af825f79ced5a619b2cf662abe0dd9d635582e4
Bow Vernon <bowvernon110391@gmail.com>
2020-07-20 23:29:20 +0700

    Finish BrowseRekamBaCacah

--- src/views/BrowseRekamBaCacah.vue
+++ src/views/BrowseRekamBaCacah.vue
@@ -139,14 +139,16 @@
         <!-- Modal utk rekam data -->
         <b-modal
             id="modal-ba-cacah"
+            ref="modal"
             size="xl"
             title="Rekam Berita Acara Pencacahan"
             footer-bg-variant="light"
             header-bg-variant="light"
+            @hide="penetapan_id=[null], bast_id=[null]"
         >
             <!-- footer -->
             <template #modal-footer>
-                <b-button size="sm" variant="primary">
+                <b-button size="sm" variant="primary" @click="createBACacah">
                     <font-awesome-icon icon="save" />
                     Rekam
                 </b-button>
@@ -186,8 +188,112 @@
             <b-row>
                 <!-- Select Penetapan -->
                 <b-col md="6">
+                    <b-form-group>
+                        <template #label>
+                            Pilih Penetapan <b-button variant="dark" class="shadow" size="sm" @click="penetapan_id.push(null)">Tambah</b-button>
+                        </template>
+
+                        <!-- List of select penetapan -->
+                        <div v-for="(p_id, index) in penetapan_id" :key="index" class="my-1">
+                            <b-input-group>
+                                <select-penetapan 
+                                    class="flex-grow-1"
+                                    :search-on-empty="!p_id"
+                                    :value="p_id" 
+                                    @input="e => penetapan_id[index] = e" 
+                                />
+                                <b-button variant="danger" class="shadow" size="sm" @click="penetapan_id.splice(index,1)">
+                                    <font-awesome-icon icon="trash-alt"/>
+                                </b-button>
+                            </b-input-group>
+                        </div>
+                    </b-form-group>
+                </b-col>
+
+                <!-- Select BAST -->
+                <b-col md="6">
+                    <b-form-group>
+                        <template #label>
+                            Pilih BAST <b-button variant="dark" class="shadow" size="sm" @click="bast_id.push(null)">Tambah</b-button>
+                        </template>
+
+                        <!-- List of select penetapan -->
+                        <div v-for="(p_id, index) in bast_id" :key="index" class="my-1">
+                            <b-input-group>
+                                <select-bast 
+                                    class="flex-grow-1"
+                                    :search-on-empty="!p_id"
+                                    :value="p_id" 
+                                    @input="e => bast_id[index] = e" 
+                                />
+                                <b-button variant="danger" class="shadow" size="sm" @click="bast_id.splice(index,1)">
+                                    <font-awesome-icon icon="trash-alt"/>
+                                </b-button>
+                            </b-input-group>
+                        </div>
+                    </b-form-group>
                 </b-col>
             </b-row>
+            <!-- Pilih pejabat + pelaksana? -->
+            <b-row>
+                
+                <b-col md="6">
+                    <b-form-group>
+                        <template #label>
+                            Pilih Pejabat (yang mengetahui)
+                        </template>
+
+                        <!-- List of select penetapan -->
+                        <select-user
+                            :search-on-empty="!pejabat_id"
+                            v-model="pejabat_id"
+                        />
+                    </b-form-group>
+                </b-col>
+                <b-col md="6">
+                    <b-form-group>
+                        <template #label>
+                            Pilih Pelaksana <b-button variant="dark" class="shadow" size="sm" @click="pelaksana_id.push(null)">Tambah</b-button>
+                        </template>
+
+                        <!-- List of select user -->
+                        <div v-for="(p_id, index) in pelaksana_id" :key="index" class="my-1">
+                            <b-input-group>
+                                <select-user 
+                                    role="tpp.pelaksana"
+                                    class="flex-grow-1"
+                                    :search-on-empty="!p_id"
+                                    :value="p_id" 
+                                    @input="e => pelaksana_id[index] = e" 
+                                />
+                                <b-button variant="danger" class="shadow" size="sm" @click="pelaksana_id.splice(index,1)">
+                                    <font-awesome-icon icon="trash-alt"/>
+                                </b-button>
+                            </b-input-group>
+                        </div>
+                    </b-form-group>
+                </b-col>
+            </b-row>
+
+            <!-- the paginated browser in offline mode? -->
+            <hr>
+            <b-form-group :label="`AWB Terpilih: (${selectedAwb.length})`">
+                <paginated-browser
+                    manual
+                    ref="browserModal"
+                    @data-request="onRequestSelectedAwb"
+                >
+                    <template #default="{ data, pagination }">
+                        <awb-flexi-table
+                            :showBc11="false"
+                            :items="data"
+
+                            :per-page="pagination.number"
+                            :current-page="pagination.page"
+                            />
+                    </template>
+                </paginated-browser>
+            </b-form-group>
         </b-modal>
     </div>
 </template>
@@ -197,6 +303,10 @@ import PaginatedBrowser from '@/components/PaginatedBrowser'
 import AwbFlexiTable from '@/components/AwbFlexiTable'
 
 import Datepicker from '@/components/Datepicker'
+
+import SelectPenetapan from '@/components/SelectPenetapan'
+import SelectBast from '@/components/SelectBast'
+import SelectUser from '@/components/SelectUser'
 
 import axiosErrorHandler from '../mixins/axiosErrorHandler'
 import niceties from '../mixins/niceties'
@@ -213,7 +323,10 @@ export default {
     components: {
         AwbFlexiTable,
         PaginatedBrowser,
-        Datepicker
+        Datepicker,
+        SelectPenetapan,
+        SelectBast,
+        SelectUser
     },
 
     data() {
@@ -231,13 +344,13 @@ export default {
             pejabat_id: null,
 
             // pelaksana_id (s)
-            pelaksana_id: [],
+            pelaksana_id: [null],
 
             // penetapan_id (s)
-            penetapan_id: [],
+            penetapan_id: [null],
 
             // bast_id (s)
-            bast_id: []
+            bast_id: [null]
         }
     },
 
@@ -255,6 +368,12 @@ export default {
 
     methods: {
         ...mapMutations(['setBusyState']),
+
+        onRequestSelectedAwb({q, spinner, vm}) {
+            var filtered = this.selectedAwb.filter(e => this.filterAwb(e, q))
+            vm.setData(filtered)
+            vm.setTotal(filtered.length)
+        },
 
         // load data from backend
         loadData (vm, q) {
@@ -286,20 +405,28 @@ export default {
         },
 
         // when bast is recorded
-        createBASTSpecific () {
+        createBACacah () {
             this.setBusyState(true)
 
             // call api
-            this.api.createBASTSpecific({
-                nomor_lengkap_dok: this.no_bast,
-                tgl_dok: this.tgl_bast,
-                entry_manifest: this.selected,
-                ex_p2: true
+            this.api.createBACacah({
+                nomor_lengkap: this.nomor_ba_cacah,
+                tgl_dok: this.tgl_ba_cacah,
+                nomor_st: this.nomor_st,
+                tgl_st: this.tgl_st,
+
+                pejabat_id: this.pejabat_id,
+
+                penetapan_id: this.penetapan_id,
+                bast_id: this.bast_id,
+                pelaksana_id: this.pelaksana_id,
+                
+                entry_manifest_id: this.selected,
             })
             .then(e => {
                 this.setBusyState(false)
                 // show toast
-                this.showToast('Done', `Berhasil merekam BAST #${e.data.id} berisi ${e.data.total} AWB`, 'success')
+                this.showToast('Done', `Berhasil merekam BA Cacah  #${e.data.id} nomor ${e.data.nomor_lengkap} tanggal ${e.data.tgl_dok} berisi ${e.data.total} AWB`, 'success')
 
                 // refresh and clear selection
                 this.$nextTick(() => {
