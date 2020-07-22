27ec68cee7cd7b68e475911265865a102547017f
Bow Vernon <bowvernon110391@gmail.com>
2020-07-22 08:38:45 +0700

    change refresh icon

--- src/views/ViewEntryManifest.vue
+++ src/views/ViewEntryManifest.vue
@@ -9,7 +9,7 @@
                     <!-- some controls here? -->
                     <div class="flex-grow-1 text-right">
                         <b-button size="sm" variant="success" class="shadow my-2" @click="loadEntryManifest">
-                            <font-awesome-icon icon="spinner" pulse/>
+                            <font-awesome-icon icon="sync"/>
                             Refresh
                         </b-button>
                     </div>
