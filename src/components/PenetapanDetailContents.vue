57e09b3ff7767f4e826883961065d9672e5ec8f0
Bow Vernon <bowvernon110391@gmail.com>
2020-09-14 14:59:19 +0700

    add gate-in based on nd as per #16

--- src/components/PenetapanDetailContents.vue
+++ src/components/PenetapanDetailContents.vue
@@ -14,9 +14,33 @@
                 :showBcp="bdnMode"
 
                 show-number
+
+                :appendFields="[{ key: 'status', class: 'text-center' }]"
             >
 
-                <!-- add numbering? -->
+                <!-- add status field? -->
+                <template #cell(status)="{ item }">
+                    <b-badge v-if="item.short_last_status"
+                    :variant="badgeVariant(item.short_last_status.status)"
+                    >
+                    {{ item.short_last_status.status }}
+                    </b-badge>
+                </template>
+
+                <!-- nope, add gate in button to those who's not gated in yet -->
+                <template #additional-controls="{ row }">
+                    <!-- show only when awb is not gated in and last status is penetapan -->
+                    <b-button
+                        v-if="row.item.short_last_status.status == 'PENETAPAN' && !row.item.waktu_gate_in"
+                        variant="dark"
+                        size="sm"
+                        @click="prosesGateIn(row.item)"
+                        class="shadow my-1"
+                        v-b-tooltip.hover title="Gate In AWB"
+                    >
+                        <font-awesome-icon icon="warehouse"/>
+                    </b-button>
+                </template>
                 
             </awb-flexi-table>
         </template>
@@ -25,6 +49,7 @@
 
 <script>
 import axiosErrorHandler from '../mixins/axiosErrorHandler'
+import niceties from '../mixins/niceties'
 import { mapGetters, mapMutations } from 'vuex'
 import PaginatedBrowser from '@/components/PaginatedBrowser'
 import TableDataAwal from '@/components/TableDataAwal'
@@ -32,7 +57,7 @@ import AwbFlexiTable from '@/components/AwbFlexiTable'
 
 
 export default {
-    mixins: [ axiosErrorHandler ],
+    mixins: [ axiosErrorHandler, niceties ],
 
     props: {
         id: {
@@ -68,6 +93,40 @@ export default {
                 spinner(false)
                 this.handleError(e)
             })
+        },
+
+        // proses gate in
+        async prosesGateIn(awb) {
+            console.log('Gate in: ', awb)
+            var answer = await this.$bvModal.msgBoxConfirm(
+                `Gate In (${awb.hawb})?`,
+                {
+                    title: 'Konfirmasi',
+                    size: 'sm',
+                    buttonSize: 'sm',
+                    okVariant: 'danger',
+                    centered: true
+                }
+            )
+
+            if (answer) {
+                this.setBusyState(true)
+                // do shit here
+                this.api.gateInAwb(awb.id)
+                .then(e => {
+                    this.setBusyState(false)
+                    // success? show toast
+                    this.showToast(`AWB Gated In (${e.data.bcp})`, `AWB ${awb.hawb} telah digate-in, nomor BCP: ${e.data.bcp}`, 'success')
+                    this.$nextTick(() => {
+                        // refresh browser
+                        this.$refs.browser.stayAtCurrentPage(0)
+                    })
+                })
+                .catch(e => {
+                    this.setBusyState(false)
+                    this.handleError(e)
+                })
+            }
         }
     },
 
